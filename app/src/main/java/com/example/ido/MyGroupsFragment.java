c1b0b63ea76bb723db4db6784960f53d7d99875e
mihalova <dominika.mihalovaa@gmail.com>
2020-04-06 13:00:38 +0200

    list of groups

--- app/src/main/java/com/example/ido/MyGroupsFragment.java
+++ app/src/main/java/com/example/ido/MyGroupsFragment.java
@@ -1,9 +1,11 @@
 package com.example.ido;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,8 @@ public class MyGroupsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my_groups, container,false);
+
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(R.string.groups);
 
         final ArrayList<Group> groups = new ArrayList<>();
 
